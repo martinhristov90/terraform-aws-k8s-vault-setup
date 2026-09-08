ba0048afe0bca8dadcb2232094538d712189c69e
Martin Hristov <mhristov@hashicorp.com>
2026-09-08 14:03:02 +0300

    Increase token_ttl from 140 to 150

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "vault_aws_auth_backend_role" "demo_sa_role" {
   role                     = var.ROLE_NAME
   auth_type                = "iam"
   bound_iam_principal_arns = [var.ALLOWED_ARN_ROLE_LOGIN] # Example: arn:aws:iam::123361688033:role/consume-pod-role-bright-halibut
-  token_ttl                = 140
+  token_ttl                = 150
   token_max_ttl            = 300
   token_policies           = [vault_policy.aws_secrets.name]
 }
